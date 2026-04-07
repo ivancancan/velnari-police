e395395457081a07c19335c48bcf48fbc810bd4c
Ivan  Rdz <ivanrdz@192.168.1.92>
2026-04-07 14:14:20 -0600

    fix: register UnitLocationHistoryEntity and IncidentEntity in UnitsModule

--- apps/api/src/modules/units/units.module.ts
+++ apps/api/src/modules/units/units.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UnitEntity } from '../../entities/unit.entity';
+import { UnitLocationHistoryEntity } from '../../entities/unit-location-history.entity';
+import { IncidentEntity } from '../../entities/incident.entity';
 import { UnitsService } from './units.service';
 import { UnitsController } from './units.controller';
 import { RealtimeModule } from '../realtime/realtime.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UnitEntity]), RealtimeModule],
+  imports: [TypeOrmModule.forFeature([UnitEntity, UnitLocationHistoryEntity, IncidentEntity]), RealtimeModule],
   controllers: [UnitsController],
   providers: [UnitsService],
   exports: [UnitsService],
