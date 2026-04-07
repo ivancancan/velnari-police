6329ec24a4bf4a475810d4cf084309d7c5a18939
Ivan  Rdz <ivanrdz@192.168.1.92>
2026-04-06 23:37:37 -0600

    chore: add shared config package (tsconfig, eslint)

---
+++ packages/config/eslint-base.js
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+  },
+};
